6fd3817e5a851bfd772605e3c6fdfd24e3228708
John Mellor-Crummey <johnmc@rice.edu>
2019-02-17 22:53:40 -0600

    changes to build system

--- scripts/boost-install.sh
+++ scripts/boost-install.sh
@@ -3,7 +3,7 @@ wget http://downloads.sourceforge.net/project/boost/boost/1.61.0/boost_1_61_0.zi
 popd
 unzip tar/boost_1_61_0.zip
 cd boost_1_61_0
-./bootstrap.sh --with-toolset=intel
+./bootstrap.sh --with-toolset=intel-linux
 ./b2 \
 	--with-system \
 	--with-thread \
