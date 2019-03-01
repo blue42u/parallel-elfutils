705fbcf7a8bcbb7a15936fb5cefd4b8ff17d7d92
blue42u <blue42u@users.noreply.github.com>
2019-02-28 22:56:14 -0600

    Update the environment to be just module loading and load the proper version of Git.

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -2,4 +2,4 @@ module load GCCcore/6.4.0
 module load GCC/6.4.0
 module load icc/2018.2.199-GCC-6.4.0
 module load CMake/3.10.3
-export LD_LIBRARY_PATH="`pwd`/install/gcc/lib:`pwd`/install/gcc/lib64:${LD_LIBRARY_PATH}"
+module load Git/2.17.1
