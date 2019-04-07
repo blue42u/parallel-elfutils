9c6a2433222389ee6379e72285c0518482d86cbc
blue42u <blue42u@users.noreply.github.com>
2019-04-07 11:42:32 -0500

    Add a new option to make Helgrind faster, and use them in the batch run

--- cl_val.sh
+++ cl_val.sh
@@ -3,4 +3,5 @@
 #SBATCH -N 1 -n 1 -c 12
 #SBATCH -o cl_val.out
 
-make -j 12 -C tests run BIGGER=1 UNFAIR=1
+make -j 12 -C tests run BIG=1
+make -j 12 -C tests run BIGGER=1 NONE=1 HELGRIND=1 HELFAST=1
