278eb60eaf1674b8994709223b9610f46dd88810
blue42u <blue42u@users.noreply.github.com>
2019-04-19 10:36:19 -0500

    Update Dyninst, use fair in batch-mode, and supress something with GOMP and nowait

--- cl_drd.sh
+++ cl_drd.sh
@@ -3,4 +3,4 @@
 #SBATCH -N 1 -n 1 -c 12
 #SBATCH -o cl_drd.out
 
-make -j 12 -C tests run NONE=1 DRD=1 BIG=1 UNFAIR=1
+make -j 12 -C tests run NONE=1 DRD=1 BIG=1
--- cl_val.sh
+++ cl_val.sh
@@ -4,4 +4,4 @@
 #SBATCH -o cl_val.out
 
 make -j 12 -C tests run BIG=1
-make -j 12 -C tests run BIGGER=1 NONE=1 HELGRIND=1 HELFAST=1
+make -j 6 -C tests run BIGGER=1 HELFAST=1
